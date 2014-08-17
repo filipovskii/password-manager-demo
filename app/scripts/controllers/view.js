2a52f74228852fa4626b5775a4fa8310dd8acbba
filipovsky <filipovskii.off@gmail.com>
2014-08-17 19:07:14 +0400

    Page view.

--- app/scripts/controllers/view.js
+++ app/scripts/controllers/view.js
@@ -8,10 +8,34 @@
  * Controller of the passwordManagerApp
  */
 angular.module('passwordManagerApp')
-  .controller('ViewCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ViewCtrl', function ($scope, $stateParams, Pages) {
+    $scope.plain = false;
+
+    Pages.get($stateParams.pageId)
+      .then(function (o) {
+        $scope.$apply(function () {
+          $scope.page = o;
+        });
+      });
+
+
+    $scope.showPlaintext = function () {
+      $scope.plain = true;
+    };
+
+    $scope.password = function () {
+      var stars;
+
+      if (!$scope.page) {
+        return '';
+      }
+
+      if ($scope.plain) {
+        return $scope.page.password;
+      } else {
+        stars = new Array($scope.page.password.length);
+        return stars.join('*');
+      }
+    };
+
   });
